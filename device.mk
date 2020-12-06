0782c2439ceaa6a8f80988e727e4959d6358136d
KakatkarAkshay <cryllicbuster273@pixelexperience.org>
2020-12-06 14:43:51 +0100

    avicii: Import rootdir from Nord-user 10 QKQ1.200412.002 2010231954
    Change-Id: I9bfc7deed9f94eaad935be6961e0a1fb68f57995

--- device.mk
+++ device.mk
@@ -121,11 +121,42 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     Snap
 
-# Common init scripts
+# Ramdisk
 PRODUCT_PACKAGES += \
-    init.qcom.rc \
+    dump_sniffer.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc
+    launch_sniffer.sh \
+    qca6234-service.sh \
+    terminate_sniffer.sh \
+    ftm_power_config.sh \
+    init.msm.usb.configfs.rc \
+    init.oem.debug.rc \
+    init.oem.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    vendor.oem_ftm.rc \
+    vendor.oem_ftm_svc_disable.rc \
+    fstab.qcom \
+    fstab_nodata.qcom \
+    ueventd.qcom.rc \
 
 # Display
 PRODUCT_PACKAGES += \
