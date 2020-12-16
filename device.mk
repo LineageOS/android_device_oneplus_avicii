63f8ba13c95e2361ec08e43313e9dd27d16cf811
Pawin Vongmasa <pawin@google.com>
2020-12-16 05:35:02 +0000

    avicii: Build Codec2 packages
    Change-Id: I291248b27e903b23be44b57c4fc33373d3e6712e

--- device.mk
+++ device.mk
@@ -377,6 +377,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml 
 
+# Media - Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
