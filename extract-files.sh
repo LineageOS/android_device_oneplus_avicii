7c2580464d204e236df16a91240bb82c1270db8d
Bruno Martins <bgcngm@gmail.com>
2023-12-05 15:02:12 +0000

    avicii: Allow extracting only firmware images
    Change-Id: Icab3f013661304e3a9ec89b0816a8139f6b8e8e5

--- extract-files.sh
+++ extract-files.sh
@@ -27,11 +27,15 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
+ONLY_FIRMWARE=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
+        --only-firmware )
+                ONLY_FIRMWARE=true
+                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -92,7 +96,9 @@ function blob_fixup() {
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
 extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
 
